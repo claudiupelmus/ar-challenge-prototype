215ec3ea68f1aad10368676290262dc5e794d7c3
Claudiu Adrian Pelmus <cpelmus@gabriels.net>
2019-10-15 15:56:06 +0300

    Small test for basic video stream capture & display

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,37 @@
-console.log("App loaded!");
+document.addEventListener("DOMContentLoaded", event => {
+	getStream();
+});
+
+const getStream = async () => {
+	const videoContainer = document.querySelector('video');
+	const constraints = {
+		video: {
+			width: { 
+				min: 400,
+				ideal: 600,
+				max: 600,
+			},
+			height: {
+				min: 400,
+				ideal: 600,
+				max: 600,
+			},
+			facingMode: {
+				exact: "environment"
+			}
+		}
+	};
+
+	try {
+		if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
+			const videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+			videoContainer.srcObject = videoStream;
+
+			videoContainer.play();
+		}
+	}
+	catch (err) {
+		console.warn(`There was an error: ${err.message.toLowerCase()}`);
+	}
+};
