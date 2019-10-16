f4dfa8f332f2e0cc6395ce6193b46509b5792bc6
Claudiu Pelmus <cpelmus@gabriels.net>
2019-10-16 23:19:56 +0300

    Test9

--- public/js/places.js
+++ public/js/places.js
@@ -47,47 +47,49 @@ const setLocations = () => {
 	const scene = document.querySelector("a-scene");
 
 	navigator.geolocation.getCurrentPosition(position => {
-		for (let location of locations) {
-			const latitude = location.coords.lat;
-			const longitude = location.coords.lng;
+		setTimeout(() => {
+			for (let location of locations) {
+				const latitude = location.coords.lat;
+				const longitude = location.coords.lng;
 
-			// add place icon
-			const icon = document.createElement("a-image");
-			icon.setAttribute("gps-entity-place", `latitude: ${latitude}; longitude: ${longitude}`);
-			icon.setAttribute("name", location.name);
-			icon.setAttribute("value", location.name);
-			icon.setAttribute("src", "../assets/map-marker.png");
+				// add place icon
+				const icon = document.createElement("a-image");
+				icon.setAttribute("gps-entity-place", `latitude: ${latitude}; longitude: ${longitude}`);
+				icon.setAttribute("name", location.name);
+				icon.setAttribute("value", location.name);
+				icon.setAttribute("src", "../assets/map-marker.png");
 
-			icon.setAttribute("scale", "10, 10");
+				icon.setAttribute("scale", "10, 10");
 
-			icon.addEventListener("loaded", () => window.dispatchEvent(new CustomEvent("gps-entity-place-loaded")));
+				icon.addEventListener("loaded", () => window.dispatchEvent(new CustomEvent("gps-entity-place-loaded")));
 
-			const clickListener = (ev) => {
-				ev.stopPropagation();
-				ev.preventDefault();
+				const clickListener = (ev) => {
+					ev.stopPropagation();
+					ev.preventDefault();
 
-				const name = ev.target.getAttribute("name");
+					const name = ev.target.getAttribute("name");
 
-				const el = ev.detail.intersection && ev.detail.intersection.object.el;
+					const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-				if (el && el === ev.target) {
-					const label = document.createElement("span");
-					const container = document.createElement("div");
-					container.setAttribute("id", "place-label");
-					label.innerText = name;
-					container.appendChild(label);
-					document.body.appendChild(container);
+					if (el && el === ev.target) {
+						const label = document.createElement("span");
+						const container = document.createElement("div");
+						container.setAttribute("id", "place-label");
+						label.innerText = name;
+						container.appendChild(label);
+						document.body.appendChild(container);
 
-					setTimeout(() => {
-						container.parentElement.removeChild(container);
-					}, 1500);
-				}
-			};
+						setTimeout(() => {
+							container.parentElement.removeChild(container);
+						}, 1500);
+					}
+				};
 
-			icon.addEventListener("click", clickListener);
-			
-			scene.appendChild(icon);
-		}
+				icon.addEventListener("click", clickListener);
+				
+				scene.appendChild(icon);
+			}
+		}, 5000);
 	},
 	(err) => console.error(`An error occured: ${err}`),
 	geolocationOptions);
